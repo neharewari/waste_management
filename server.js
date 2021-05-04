9b30511fe95f61b4017cb3d8ed500b0922797fc7
Reyaz <reyazahamed98@gmail.com>
2021-05-04 21:50:57 +0530

    added routes, established mongoose connection & Seller schema added

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const connectDB = require("./config/db");
+var cors = require("cors");
+
+//connect database
+connectDB();
+
+//Init Middleware
+app.use(express.json({ extended: false }));
+app.use(cors());
+
+//define routes
+app.use("/feedback", require("./routes/feedback"));
+app.use("/users", require("./routes/users"));
+app.use("/auth", require("./routes/auth"));
+app.use("/seller", require("./routes/seller"));
+app.use("/rider", require("./routes/rider"));
+app.use("/vendor", require("./routes/vendor"));
+
+//TODO: can add heroku deploy code
+
+//run the server
+const port = process.env.PORT || 1210;
+app.listen(port, () => console.log(`Server started on port ${port}`));
